5756b09922d9c9955d0c1bf08a7cae12bbd87cbd
Kaka <kaka7939@kakao.com>
2022-07-31 17:17:13 +0900

    update P2022_07_12 Clock.java

--- src/p2022_07_12/Clock.java
+++ src/p2022_07_12/Clock.java
@@ -24,7 +24,6 @@ public class Clock extends JFrame implements Runnable{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
